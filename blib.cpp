99d524bf3af403041fa5cfef4c450b3010f0c1de
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-09-21 16:43:54 +0200

    🧩ecs working, but not super well tested

--- blib.cpp
+++ blib.cpp
@@ -10,13 +10,71 @@ public:
     int y;
 };
 
-int main()
+class timer : public blib::component
 {
-    std::cout << "Hello World!\n";
+public:
+    timer() : t(0.0f) {}
+    void update(float dt) override { t += dt; }
+    float get_t() const { return t; }
 
+private:
+    float t;
+};
+
+class random_spawner : public blib::component
+{
+public:
+    random_spawner() {}
+    void update(float dt) override;
+private:
+};
+
+int main()
+{    
     blib::entity_container ec;
-    auto& e = ec.create();
-    auto& t = e.create_component<transform>(1, 4);
-    t.x = 1;
-    t.y = 2;
+    auto& e = ec.create();    
+    e.set_name("de entity");
+    std::cout << "e.valid = " << e.valid() << std::endl;
+    
+    auto& t = e.create_component<transform>(41, 41);
+    std::cout << "t = [" << t.x << ", " << t.y << "]" << std::endl;
+
+    t.x = 63;
+    t.y = 89;
+    std::cout << "t = [" << t.x << ", " << t.y << "]" << std::endl;
+
+    blib::entity invalid_entity;
+    std::cout << "invalid_entity.valid = " << invalid_entity.valid() << std::endl;
+
+    blib::entity_id invalid_id;
+    // auto& error_entity = ec.get(invalid_id);    // This will result in a run time error! 
+    // std::cout << "e0.valid = " << error_entity.valid() << std::endl;
+
+    auto* error_entity_ptr = ec.try_get(invalid_id);
+    if(error_entity_ptr)
+        std::cout << "error_entity_ptr->valid = " << error_entity_ptr->valid() << std::endl;
+
+    auto e_id = e.id();
+    auto& e_again = ec.get(e_id);
+    std::cout << "e_again.name() = " << e_again.name() << std::endl;
+
+    auto& tm = e_again.create_component<timer>();
+    e_again.update(1.0f);
+    ec.update(1.0f);
+    std::cout << "tm.get_t() = " << tm.get_t() << std::endl;
+
+    auto& t_again = e_again.get_component<transform>();
+    std::cout << "t_again = [" << t_again.x << ", " << t_again.y << "]" << std::endl;
+
+    e_again.destroy_component<transform>();
+    auto* t_again_ptr = e_again.try_get_component<transform>();
+    if (t_again_ptr)
+        std::cout << "t_again_ptr = [" << t_again_ptr->x << ", " << t_again_ptr->y << "]" << std::endl;
+
+    ec.destroy(e_again.id());
+    ec.update(0.0f);
+
+    auto* not_here_entity_ptr = ec.try_get(e_id);
+    if (t_again_ptr)
+        std::cout << "not_here_entity_ptr.valid = " << not_here_entity_ptr->valid() << std::endl;
 }
--- blib_ec.h
+++ blib_ec.h
@@ -23,15 +23,17 @@ class component
 public:	
 	virtual void update(float dt) {}
 
-private:
+protected:
 	entity* m_parent = nullptr;
 };
 
 class entity
 {
-	friend class entity_container;
+	friend class entity_container;	
+	explicit entity(entity_id id) : m_id(id) { }
 
-public:
+public:	
+	entity() {}
 	entity(const entity&) = delete;
 	entity(entity&&) = default;
 	entity& operator=(const entity&) = delete;
@@ -46,8 +48,19 @@ public:
 	template<class T, typename... Args>
 	T& create_component(Args&&... args);
 
-private:
-	entity() {}
+	template<class T>
+	T& get_component();
+
+	template<class T>
+	T* try_get_component();
+
+	template<class T>
+	void destroy_component();
+
+	bool valid() const { return m_id.id != 0; }
+	void update(float dt);
+
+private:	
 	entity_id m_id = {};
 	std::string m_name = {};
 	uint64_t m_tag = {};
@@ -58,45 +71,113 @@ class entity_container
 {
 public:
 	entity& create();
-	const entity& get(entity_id id) const;
 	entity* try_get(entity_id id);
 	entity& get(entity_id id);
 	void destroy(entity_id id);
 	void update(float dt);
+
 private:
+	void clean();
 	std::unordered_map<uint64_t, entity> m_entities;
 	uint64_t m_next_id = 0;
 };
 
+////////////////////////////////////////////////////////////////////////////////
+//// 
+////						Implementation
+////
+////////////////////////////////////////////////////////////////////////////////
 
-// Implementation
 
-
+////////////////////////////////////////////////////////////////////////////////
+////							Entity
+////////////////////////////////////////////////////////////////////////////////
 
 template<class C, typename... Args >
-inline C& entity::create_component(Args&&... args)
+inline C& blib::entity::create_component(Args&&... args)
 {
 	auto c = std::make_unique<C>(std::forward<Args>(args)...);
 	c->m_parent = this;
 	auto& ref = m_components.emplace_back(move(c));
 	return *(static_cast<C*>(ref.get()));
-	//return *static_cast<C*>(m_components.back().get());
 }
+
+template<class T>
+inline T& blib::entity::get_component()
+{
+	T* found = try_get_component<T>();
+	assert(found != nullptr);
+	return *found;
+}
+
+template<class T>
+inline T* entity::try_get_component()
+{
+	for (auto& c : m_components)
+	{
+		T* found = dynamic_cast<T*>(c.get());
+		if (found)
+			return found;
+	}
+	return nullptr;
+}
+
+template<class T>
+inline void entity::destroy_component()
+{
+	for (auto it = m_components.begin();  it != m_components.end(); ++it)
+	{
+		T* found = dynamic_cast<T*>(it->get());
+		if (found) {
+			m_components.erase(it);
+			break;
+		}
+	}
+}
+
+void blib::entity::update(float dt)
+{
+	for (auto& c : m_components)
+		c.get()->update(dt);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////						Entity Container
+////////////////////////////////////////////////////////////////////////////////
 
 inline entity& blib::entity_container::create()
 {
-	entity e;
-	e.m_id.id = ++m_next_id;
-	m_entities.emplace(++m_next_id, entity());
-	return m_entities[e.m_id.id];
+	m_next_id++;
+	m_entities.emplace(m_next_id, entity({ m_next_id }));
+	return m_entities[m_next_id];
 }
 
-inline const entity& entity_container::get(entity_id id) const
+inline entity& blib::entity_container::get(entity_id id)
 {
 	auto itr = m_entities.find(id.id);
 	assert(itr != m_entities.end());
 	return itr->second;
 }
+
+inline void blib::entity_container::update(float dt)
+{
+	for (auto& e : m_entities)
+		if(e.second.valid())
+			e.second.update(dt);
+
+	clean();
+}
+
+inline void blib::entity_container::clean()
+{
+	for (auto it = m_entities.begin(); it != m_entities.end();)
+		if (!it->second.valid())
+			it = m_entities.erase(it);
+		else
+			++it;
+}
+
 
 inline entity* blib::entity_container::try_get(entity_id id)
 {
@@ -106,5 +187,11 @@ inline entity* blib::entity_container::try_get(entity_id id)
 	return nullptr;
 }
 
+inline void entity_container::destroy(entity_id id)
+{
+	auto itr = m_entities.find(id.id);
+	if (itr != m_entities.end())
+		itr->second.m_id = {};
+}
 
 }
